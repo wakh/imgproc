066ea4f1825ca7af4c7e807173c2f53810d3645b
wakh <khamwa92@gmail.com>
2021-05-12 03:24:27 -0700

    Project modules init

---
+++ src/tests/helpers/reporter.ts
@@ -0,0 +1,24 @@
+import {DisplayProcessor, SpecReporter, StacktraceOption}
+  from 'jasmine-spec-reporter';
+import SuiteInfo = jasmine.JasmineDoneInfo;
+
+/** Class representing a Custom Processor */
+class CustomProcessor extends DisplayProcessor {
+  /**
+    * Display Logs Result
+    * @param {SuiteInfo} info - Information about the full Jasmine suite
+    * @param {string} log - Results
+    * @return {string} Return results
+    */
+  public displayJasmineStarted(info: SuiteInfo, log: string): string {
+    return `${log}`;
+  }
+}
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(new SpecReporter({
+  spec: {
+    displayStacktrace: StacktraceOption.NONE,
+  },
+  customProcessors: [CustomProcessor],
+}) as jasmine.CustomReporter);
